8fe6e934040c4e2c2c957a562591d7b2eda2ab15
RubD <rubendries@gmail.com>
2020-06-13 17:17:22 -0400

    add miniscript visium

--- inst/examples/mini_visium_test/mini_visium_deg_v0.3.5_200612.R
+++ inst/examples/mini_visium_test/mini_visium_deg_v0.3.5_200612.R
@@ -5,25 +5,25 @@ library(Giotto)
 # createGiottoInstructions(python_path = '/your/path')
 
 ## Giotto 0.3.5 ##
-## mini-test seqFish Giotto 0.3.5 ##
+## mini-test Visium Brain Giotto 0.3.5 ##
 
-temp_dir = '/your/path/to/results'
+temp_dir = '/path/to/your/temporary/directory/results'
 
 ## 1. giotto object ####
 expr_path = system.file("extdata", "visium_DG_expr.txt", package = 'Giotto')
 loc_path = system.file("extdata", "visium_DG_locs.txt", package = 'Giotto')
 
-# default giotto object
+# default
 brain_small <- createGiottoObject(raw_exprs = expr_path, spatial_locs = loc_path)
+
 # with instructions (e.g. specific python path)
 # myinstructions = createGiottoInstructions(python_path = '/your/path')
 # brain_small <- createGiottoObject(raw_exprs = expr_path, spatial_locs = loc_path, instructions = myinstructions)
 showGiottoInstructions(brain_small)
 
-# giotto image
 
 ## 1. read image
-png_path = '/Volumes/Ruben_Seagate/Dropbox/Packages/R_packages/Github/Giotto/inst/extdata/deg_image.png'
+png_path = system.file("extdata", "deg_image.png", package = 'Giotto')
 mg_img = magick::image_read(png_path)
 
 ## 2. test and modify image alignment
@@ -37,7 +37,7 @@ mypl_image
 ## 3. add images to Giotto object ##
 image_list = list(orig_png)
 brain_small = addGiottoImage(gobject = brain_small,
-                           images = image_list)
+                             images = image_list)
 showGiottoImageNames(brain_small)
 
 
@@ -53,11 +53,11 @@ filterCombinations(brain_small,
                    min_det_genes_per_cell = c(100, 200, 100, 200))
 
 brain_small <- filterGiotto(gobject = brain_small,
-                             expression_threshold = 1,
-                             gene_det_in_min_cells = 50,
-                             min_det_genes_per_cell = 100,
-                             expression_values = c('raw'),
-                             verbose = T)
+                            expression_threshold = 1,
+                            gene_det_in_min_cells = 50,
+                            min_det_genes_per_cell = 100,
+                            expression_values = c('raw'),
+                            verbose = T)
 brain_small <- normalizeGiotto(gobject = brain_small, scalefactor = 6000, verbose = T)
 brain_small <- addStatistics(gobject = brain_small)
 
@@ -79,7 +79,7 @@ brain_small <- doLeidenCluster(gobject = brain_small, resolution = 0.4, n_iterat
 plotUMAP(gobject = brain_small, cell_color = 'leiden_clus', show_NN_network = T, point_size = 2.5)
 
 ## 5. co-visualize ####
-spatDimPlot(gobject = brain_small, cell_color = 'leiden_clus', show_image = T,
+spatDimPlot(gobject = brain_small, cell_color = 'leiden_clus',
             dim_point_size = 2, spat_point_size = 2.5)
 
 
@@ -100,26 +100,53 @@ plotMetaDataHeatmap(brain_small, selected_genes = topgenes_scran,
                     metadata_cols = c('leiden_clus'))
 
 
-## 7. cell type annotation ####
+
+## 7. annotation ####
+
+### 7.1 spot annotation ####
 clusters_cell_types = c('Gfap_cells', 'Tbr1_cells', 'Tcf7l2_cells', 'Wfs1_cells', 'Nptxr_cells')
 names(clusters_cell_types) = 1:5
 brain_small = annotateGiotto(gobject = brain_small, annotation_vector = clusters_cell_types,
-                          cluster_column = 'leiden_clus', name = 'cell_types')
+                             cluster_column = 'leiden_clus', name = 'cell_types')
 spatDimPlot(gobject = brain_small, cell_color = 'cell_types', spat_point_size = 3, dim_point_size = 3)
 
 
 
-## 7. spatial grid ####
+
+### 7.2 spatial enrichment: cell type distribution ####
+
+# known markers for different mouse brain cell types:
+# Zeisel, A. et al. Molecular Architecture of the Mouse Nervous System. Cell 174, 999-1014.e22 (2018).
+
+## cell type signatures ##
+## combination of all marker genes identified in Zeisel et al
+sign_matrix_path = system.file("extdata", "sig_matrix.txt", package = 'Giotto')
+brain_sc_markers = data.table::fread(sign_matrix_path) # file don't exist in data folder
+sig_matrix = as.matrix(brain_sc_markers[,-1]); rownames(sig_matrix) = brain_sc_markers$Event
+
+## enrichment tests
+visium_brain = createSpatialEnrich(brain_small, sign_matrix = sig_matrix, enrich_method = 'PAGE') #default = 'PAGE'
+
+## heatmap of enrichment versus annotation (e.g. clustering result)
+cell_types = colnames(sig_matrix)
+plotMetaDataCellsHeatmap(gobject = visium_brain,
+                         metadata_cols = 'leiden_clus',
+                         value_cols = cell_types,
+                         spat_enr_names = 'PAGE',x_text_size = 8, y_text_size = 8)
+
+
+
+## 8. spatial grid ####
 brain_small <- createSpatialGrid(gobject = brain_small,
-                              sdimx_stepsize = 300,
-                              sdimy_stepsize = 300,
-                              minimum_padding = 50)
+                                 sdimx_stepsize = 300,
+                                 sdimy_stepsize = 300,
+                                 minimum_padding = 50)
 showGrids(brain_small)
 spatPlot(gobject = brain_small, show_grid = T, point_size = 1.5)
 
 
 
-## 8. spatial network ####
+## 9. spatial network ####
 plotStatDelaunayNetwork(gobject = brain_small, maximum_distance = 300)
 brain_small = createSpatialNetwork(gobject = brain_small, minimum_k = 2, maximum_distance_delaunay = 400)
 brain_small = createSpatialNetwork(gobject = brain_small, minimum_k = 2, method = 'kNN', k = 10)
@@ -135,7 +162,7 @@ spatPlot(gobject = brain_small, show_network = T,
 
 
 
-## 9. spatial genes ####
+## 10. spatial genes ####
 km_spatialgenes = binSpect(brain_small)
 spatGenePlot(brain_small, expression_values = 'scaled', genes = km_spatialgenes[1:6]$genes,
              point_shape = 'border', point_border_stroke = 0.1,
@@ -155,7 +182,7 @@ spatGenePlot(brain_small, expression_values = 'scaled', genes = silh_spatialgene
              cow_n_col = 2)
 
 
-## 10. spatial co-expression patterns ####
+## 11. spatial co-expression patterns ####
 ext_spatial_genes = km_spatialgenes[1:100]$genes
 spat_cor_netw_DT = detectSpatialCorGenes(brain_small,
                                          method = 'network', spatial_network_name = 'Delaunay_network',
@@ -179,7 +206,7 @@ spatCellPlot(brain_small,
 
 
 
-## 11. spatial HMRF domains ####
+## 12. spatial HMRF domains ####
 hmrf_folder = paste0(temp_dir,'/','11_HMRF/')
 if(!file.exists(hmrf_folder)) dir.create(hmrf_folder, recursive = T)
 
@@ -202,9 +229,9 @@ for(i in seq(28, 30, by = 2)) {
 }
 
 brain_small = addHMRF(gobject = brain_small,
-                   HMRFoutput = HMRF_spatial_genes,
-                   k = 8, betas_to_add = c(28),
-                   hmrf_name = 'HMRF')
+                      HMRFoutput = HMRF_spatial_genes,
+                      k = 8, betas_to_add = c(28),
+                      hmrf_name = 'HMRF')
 
 giotto_colors = getDistinctColors(8)
 names(giotto_colors) = 1:8
