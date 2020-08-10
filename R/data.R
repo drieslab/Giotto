aa8045bfd72d7439971119142ffc24b7f896670d
RubD <rubendries@gmail.com>
2020-08-09 21:55:38 -0400

    add mini dataset test

---
+++ R/data.R
@@ -0,0 +1,23 @@
+
+
+#' mini Giotto object for spatial single-cell resolution data
+#'
+#' Mini Giotto object created from the seqFISH+ data.
+#'
+#' @docType data
+#'
+#' @usage data(mini_giotto_single_cell)
+#'
+#' @format An object of class \code{"giotto"}; see \code{\link[Giotto]{createGiottoObject}}.
+#'
+#' @keywords datasets
+#'
+#' @references Eng et al. (2019) Nature
+#' (\href{https://www.nature.com/articles/s41586-019-1049-y}{PubMed})
+#'
+#'
+#' @examples
+#' data(mini_giotto_single_cell)
+#'
+#' \donttest{spatPlot(mini_giotto_single_cell)}
+"mini_giotto_single_cell"
