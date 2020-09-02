7ae4eab891082c975df5e29544661cb85c9647a2
RubD <rubendries@gmail.com>
2020-09-02 19:29:19 -0400

    update hvg docs

--- R/variable_genes.R
+++ R/variable_genes.R
@@ -21,6 +21,7 @@
 #' @return giotto object highly variable genes appended to gene metadata (fDataDT)
 #' @details
 #' Currently we provide 2 ways to calculate highly variable genes:
+#'
 #' \bold{1. high coeff of variance (COV) within groups: } \cr
 #' First genes are binned (\emph{nr_expression_groups}) into average expression groups and
 #' the COV for each gene is converted into a z-score within each bin. Genes with a z-score
@@ -34,9 +35,22 @@
 #' @examples
 #'
 #' data(mini_giotto_single_cell) # loads existing Giotto object
-#' mini_giotto <- calculateHVG(gobject = mini_giotto, zscore_threshold = 0.1, nr_expression_groups = 3)
 #'
-#' hvg_dt <- calculateHVG(gobject = mini_giotto, zscore_threshold = 0.1, nr_expression_groups = 3, return_gobject = FALSE)
+#' # update a giotto object
+#' mini_giotto_single_cell <- calculateHVG(gobject = mini_giotto_single_cell,
+#'                                         zscore_threshold = 0.1,
+#'                                         nr_expression_groups = 3)
+#'
+#' # return a data.table with the high variable genes annotated
+#' hvg_dt <- calculateHVG(gobject = mini_giotto_single_cell,
+#'                        zscore_threshold = 0.1, nr_expression_groups = 3,
+#'                        return_plot = FALSE, return_gobject = FALSE)
+#'
+#' # return the ggplot object
+#' hvg_plot <- calculateHVG(gobject = mini_giotto_single_cell,
+#'                        zscore_threshold = 0.1, nr_expression_groups = 3,
+#'                        return_plot = TRUE, return_gobject = FALSE)
+#'
 #'
 calculateHVG <- function(gobject,
                          expression_values = c('normalized', 'scaled', 'custom'),
