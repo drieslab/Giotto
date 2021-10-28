1d687fdf7cd4bc35b42890538c0d3f0c34fd880d
Junxiang Xu <chucksailors95@gmail.com>
2021-10-28 01:06:30 -0400

    deprecation

--- R/spatial_visuals.R
+++ R/spatial_visuals.R
@@ -4737,7 +4737,9 @@ dimGenePlot2D <- function(gobject,
                           genes = NULL,
                           default_save_name = 'dimGenePlot2D',
                           ...) {
-
+  
+  .Deprecated(new = "dimFeatPlot2D")
+  
   dimFeatPlot2D(gobject = gobject,
                 feat_type = 'rna',
                 feats = genes,
@@ -4756,7 +4758,9 @@ dimGenePlot2D <- function(gobject,
 #' @export
 #' @seealso \code{\link{dimGenePlot3D}}
 dimGenePlot = function(...) {
-
+  
+  .Deprecated(new = "dimGenePlot2D")
+  
   dimGenePlot2D(...)
 
 }
@@ -5024,6 +5028,8 @@ spatDimGenePlot2D <- function(gobject,
                               default_save_name = 'spatDimGenePlot2D',
                               ...) {
 
+  .Deprecated(new = "spatDimFeatPlot2D")
+  
   spatDimFeatPlot2D(gobject = gobject,
                     feat_type = 'rna',
                     feats = genes,
@@ -5047,6 +5053,9 @@ spatDimGenePlot2D <- function(gobject,
 #' @export
 #' @seealso \code{\link{spatDimGenePlot3D}}
 spatDimGenePlot = function(...) {
+  
+  .Deprecated(new = "spatDimGenePlot2D")
+  
   spatDimGenePlot2D(...)
 }
 
