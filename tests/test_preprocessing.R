164edc5a2e8ddd89025670b899b1e180a7ab81d7
Jason Yeung <yeung.jsn@gmail.com>
2022-06-01 15:23:10 -0400

    Added testthat framework

---
+++ tests/test_preprocessing.R
@@ -0,0 +1,16 @@
+# Tests for functions dependent on Giotto object
+
+expr_path = system.file("extdata", "seqfish_field_expr.txt.gz", package = 'Giotto')
+loc_path = system.file("extdata", "seqfish_field_locs.txt", package = 'Giotto')
+
+
+# Tests for Giotto object creation
+# ---------------------------------------------
+
+
+object <- createGiottoObject(raw_exprs = expr_path,
+                             spatial_locs = loc_path)
+
+test_that("Object initialization creates Giotto object", {
+  expect_is(object, "giotto")
+})
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(Giotto)
+
+test_check("Giotto")
