9c3592f4a7f72dfc6eb9fa191532a0c3c34dc1c5
Jason Yeung <yeung.jsn@gmail.com>
2022-06-02 11:33:01 -0400

    Update unit test

--- tests/testthat/test_preprocessing.R
+++ tests/testthat/test_preprocessing.R
@@ -11,5 +11,5 @@ object <- createGiottoObject(raw_exprs = expr_path,
                              spatial_locs = loc_path)
 
 test_that("Object initialization creates Giotto object", {
-  expect_is(object, "giotto")
+  expect_s4_class(object, "giotto")
 })
