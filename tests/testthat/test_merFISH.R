b3c62c8fbb17bcf54e25e3cf65b476ad53a6408c
Jason Yeung <yeung.jsn@gmail.com>
2022-06-07 10:59:08 -0400

    Add merFISH extdata

---
+++ tests/testthat/test_merFISH.R
@@ -0,0 +1,15 @@
+# Tests for functions dependent on Giotto object
+
+expr_path = system.file("extdata", "seqfish_field_expr.txt.gz", package = 'Giotto')
+loc_path = system.file("extdata", "seqfish_field_locs.txt", package = 'Giotto')
+
+
+# Tests for Giotto object creation
+# ---------------------------------------------
+
+object <- createGiottoObject(raw_exprs = expr_path,
+                             spatial_locs = loc_path)
+
+test_that("Object initialization creates Giotto object", {
+  expect_s4_class(object, "giotto")
+})
