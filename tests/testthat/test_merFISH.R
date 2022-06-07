ee59757dff2fcf291aaae1f32f4aff535c906e15
Jason Yeung <yeung.jsn@gmail.com>
2022-06-07 11:56:12 -0400

    Revert to Giotto env

--- tests/testthat/test_merFISH.R
+++ tests/testthat/test_merFISH.R
@@ -1,5 +1,10 @@
 # Tests for merFISH workflow
 
+python_path = NULL 
+if(is.null(python_path)) {
+  installGiottoEnvironment()
+}
+
 expr_path = system.file("extdata", "merFISH_3D_data_expression.txt.gz", package = 'Giotto')
 loc_path = system.file("extdata", "merFISH_3D_data_cell_locations.txt", package = 'Giotto')
 meta_path = system.file("extdata", "merFISH_3D_metadata.txt", package = 'Giotto')
