459086b632e95b6870bb346c61b6cbf6c65759ff
Jason Yeung <jy357@scc-wl3.scc.bu.edu>
2022-06-10 10:40:03 -0400

    Modify extdata dir structure

--- tests/testthat/test_merFISH.R
+++ tests/testthat/test_merFISH.R
@@ -3,9 +3,9 @@ if(is.null(python_path)) {
   installGiottoEnvironment()
 }
 
-expr_path = system.file("extdata", "merFISH_3D_data_expression.txt.gz", package = 'Giotto')
-loc_path = system.file("extdata", "merFISH_3D_data_cell_locations.txt", package = 'Giotto')
-meta_path = system.file("extdata", "merFISH_3D_metadata.txt", package = 'Giotto')
+expr_path = system.file("extdata/merFISH_3D_data", "merFISH_3D_data_expression.txt.gz", package = 'Giotto')
+loc_path = system.file("/extdata/merFISH_3D_data", "merFISH_3D_data_cell_locations.txt", package = 'Giotto')
+meta_path = system.file("extdata/merFISH_3D_data", "merFISH_3D_metadata.txt", package = 'Giotto')
 
 ### TESTS FOR MERFISH MOUSE HYPOTHALMIC PREOPTIC REGION DATASET
 # --------------------------------------------------------------
