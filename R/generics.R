9ca244e2c921476c382f55cc4540bf2f0d8ad546
jiajic <72078254+jiajic@users.noreply.github.com>
2022-09-08 22:17:34 -0400

    Update generics.R
    Fix documentation issues in devtools check

--- R/generics.R
+++ R/generics.R
@@ -9,6 +9,7 @@
 #' @title Dimensions of giotto points and polygon objects
 #' @name nrow-generic
 #' @description Get the number of rows (nrow)
+#' @include giotto_structures.R
 #' @param x giottoPoints or giottoPolygon object
 #' @aliases nrow
 #' @exportMethod nrow
@@ -39,38 +40,39 @@ setMethod('nrow', signature('giottoPolygon'), function(x) terra::nrow(x@spatVect
 # plot() S4 Generic ####
 
 #' @title Preview a Giotto spatial object
-#' @description S4 generic for previewing Giotto's image and subcellular objects.
 #' @name plot-generic
-#' @aliases plot
+#' @description S4 generic for previewing Giotto's image and subcellular objects.
+#' @include giotto_structures.R images.R
 #' @param x giotto image, giottoPolygon, or giottoPoints object
-#' @param ... additional parameters to pass
+#' @param y Not used.
+#' @param \dots additional parameters to pass
+#' @aliases plot
 #' @family plot
 #' @exportMethod plot
 setOldClass('plot')
 
 #' @describeIn plot-generic Plot \emph{magick}-based giottoImage object. ... param passes to \code{\link{plot_giottoImage_MG}}
 #' @export
-setMethod('plot', signature('giottoImage'), function(x,...) plot_giottoImage_MG(giottoImage = x,...))
+setMethod('plot', signature(x = 'giottoImage', y = 'missing'), function(x,y,...) plot_giottoImage_MG(giottoImage = x,...))
 
 #' @describeIn plot-generic Plot \emph{terra}-based giottoLargeImage object. ... param passes to \code{\link{plot_giottoLargeImage}}
 #' @export
-setMethod('plot', signature('giottoLargeImage'), function(x,...) plot_giottoLargeImage(giottoLargeImage = x,...))
+setMethod('plot', signature(x = 'giottoLargeImage', y = 'missing'), function(x,y,...) plot_giottoLargeImage(giottoLargeImage = x,...))
 
 #' @describeIn plot-generic Plot \emph{terra}-based giottoPolygon object. ... param passes to \code{\link[terra]{plot}}
 #' @export
-setMethod('plot', signature('giottoPolygon'), function(x,...) {
-  terra::plot(x = x@spatVector,...)
-})
+setMethod('plot', signature(x = 'giottoPolygon', y = 'missing'), function(x,y,...) terra::plot(x = x@spatVector,...))
 
 #' @describeIn plot-generic \emph{terra}-based giottoPoint object. ... param passes to \code{\link[terra]{plot}}
 #' @param point_size size of points when plotting giottoPoint
 #' @param feats specific features to plot within giottoPoint object (defaults to NULL, meaning all available features)
 #' @export
-setMethod('plot', signature('giottoPoints'), function(x,
-                                                      point_size = 0.1,
-                                                      feats = NULL,
-                                                      ...) {
-  
+setMethod('plot', signature(x = 'giottoPoints', y = 'missing'), function(x,
+                                                                         y,
+                                                                         point_size = 0.1,
+                                                                         feats = NULL,
+                                                                         ...) {
+
   if(is.null(feats)) terra::plot(x = x@spatVector, cex = point_size,...)
   else if(length(feats) == 1) {
     gp = x@spatVector
