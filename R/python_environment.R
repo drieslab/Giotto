1823a566633f8f6ba21f09145bf44d23eeeca21f
RubD <rubendries@gmail.com>
2022-11-17 08:52:33 -0500

    aesthetics

--- R/python_environment.R
+++ R/python_environment.R
@@ -121,7 +121,9 @@ install_giotto_environment_specific = function(packages_to_install = c('pandas',
   if(.Platform[['OS.type']] == 'unix') {
 
     conda_full_path = paste0(conda_path,'/','bin/conda')
-    reticulate::conda_create(envname = 'giotto_env',
+    installGiottoEnvironment(force_miniconda = FALSE,
+                             force_environment = TRUE,
+                             verbose = TRUE)(envname = 'giotto_env',
                              conda = conda_full_path,
                              python_version = python_version)
 
