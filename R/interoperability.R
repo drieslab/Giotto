f61ab8cf86a813e4eb518a73e57100edeb53572b
irzamsarfraz <irzam9095@gmail.com>
2023-01-01 12:38:54 -0500

    Fixed other slots for separate spe objects

--- R/interoperability.R
+++ R/interoperability.R
@@ -998,6 +998,7 @@ giottoToSpatialExperiment <- function(giottoObj){
   # Iterate over spatial units
   spatialUnits <- unique(giottoExpr$spat_unit) # a function to get spat units?
   for(su in seq(spatialUnits)){
+    message("Processing spatial unit: '", spatialUnits[su], "'")
     # Check if expression matrices exist in input object
     if(!is.null(giottoExpr)){
       message("Copying expression matrix: '", giottoExpr[1]$name, "' for spatial unit: '", spatialUnits[su], "'")
@@ -1040,123 +1041,136 @@ giottoToSpatialExperiment <- function(giottoObj){
       }
     }
 
+    # Cell Metadata to ColData
+    pData <- pDataDT(gobject = giottoObj, spat_unit = spatialUnits[su])
+    if(nrow(pData) > 0){
+      message("Copying phenotype data for spatial unit: '", spatialUnits[su], "'")
+      SummarizedExperiment::colData(spe) <- S4Vectors::DataFrame(pData, row.names = pData$cell_ID)
+    } else{
+      message("No phenotype data found in input Giotto object")
+    }
+
+    # Feature Metadata to RowData
+    fData <- fDataDT(gobject = giottoObj, spat_unit = spatialUnits[su])
+    if(nrow(fData) > 0){
+      message("Copying feature metadata for spatial unit: '", spatialUnits[su], "'")
+      SummarizedExperiment::rowData(spe) <- fData
+    } else{
+      message("No feature metadata found in input Giotto object")
+    }
+
+    # Spatial Locations to Spatial Coordinates
+    spatialLocs <- get_spatial_locations(gobject = giottoObj,
+                                         spat_unit = spatialUnits[su],
+                                         output = "data.table")
+    if(!is.null(spatialLocs)){
+      message("Copying spatial locations for spatial unit: '", spatialUnits[su], "'")
+      SpatialExperiment::spatialCoords(spe) <- data.matrix(spatialLocs[, 1:2])
+    } else{
+      message("No spatial locations found in the input Giotto object")
+    }
+
+    # DimReductions
+    giottoReductions <- list_dim_reductions(gobject = giottoObj, spat_unit = spatialUnits[su])
+    if(!is.null(giottoReductions)){
+      message("Copying reduced dimensions for spatial unit: '", spatialUnits[su], "'")
+      for(i in seq(nrow(giottoReductions))){
+        SingleCellExperiment::reducedDim(spe, giottoReductions[i]$name) <- get_dimReduction(
+          gobject = giottoObj,
+          reduction = "cells",
+          spat_unit = spatialUnits[su],
+          feat_type = giottoReductions[i]$feat_type,
+          reduction_method = giottoReductions[i]$dim_type,
+          name = giottoReductions[i]$name,
+          output = "data.table")
+      }
+    } else{
+      message("No reduced dimensions found in the input Giotto object")
+    }
+
+
+    # NN Graph
+    giottoNearestNetworks <- list_nearest_networks(gobject = giottoObj, spat_unit = spatialUnits[su])
+    if(!is.null(giottoNearestNetworks)){
+      message("Copying nearest networks for spatial unit: '", spatialUnits[su], "'")
+      for(i in seq(nrow(giottoNearestNetworks))){
+        nn_network <- get_NearestNetwork(
+          gobject = giottoObj,
+          spat_unit = spatialUnits[su],
+          nn_network_to_use = giottoNearestNetworks[i]$type,
+          network_name = giottoNearestNetworks[i]$name,
+          output = "data.table")
+
+        # SPE stores in colpairs, with col indices instead of colnames
+        cell1 <- match(nn_network$from, pData$cell_ID)
+        cell2 <- match(nn_network$to, pData$cell_ID)
+
+        SingleCellExperiment::colPair(spe, giottoNearestNetworks[i]$name) <- S4Vectors::SelfHits(
+          cell1, cell2, nnode=ncol(spe),
+          nn_network[, -1:-2] #removing from and to
+          )
+      }
+    } else{
+      message("No nearest networks found in the input Giotto object")
+    }
+
+    # Spatial Networks
+    giottoSpatialNetworks <- list_spatial_networks(gobject = giottoObj, spat_unit = spatialUnits[su])
+    if(!is.null(giottoSpatialNetworks)){
+      message("Copying spatial networks for spatial unit: '", spatialUnits[su], "'")
+      for(i in seq(nrow(giottoSpatialNetworks))){
+        sp_network <- get_spatialNetwork(
+          gobject = giottoObj,
+          spat_unit = spatialUnits[su],
+          name = giottoSpatialNetworks[i]$name,
+          output = "networkDT")
+
+        # spe stores in colpairs, with col indices instead of colnames
+        cell1 <- match(sp_network$from, pData$cell_ID)
+        cell2 <- match(sp_network$to, pData$cell_ID)
+
+        SingleCellExperiment::colPair(spe, giottoSpatialNetworks[i]$name) <- S4Vectors::SelfHits(
+          cell1, cell2, nnode=ncol(spe),
+          sp_network[, -1:-2] #removing from and to
+          )
+      }
+    } else{
+      message("No spatial networks found in the input Giotto object")
+    }
+
+    # SpatialImages - are these for each spatial unit?
+    giottoImages <- list_images(gobject = giottoObj)
+    if(!is.null(giottoImages)){
+      message("Copying spatial images")
+      for(i in seq(nrow(giottoImages))){
+        img <- get_giottoImage(
+          gobject = giottoObj,
+          image_type = giottoImages[i]$img_type,
+          name = giottoImages[i]$name)
+
+        if(!is.null(img@file_path)){
+          spe <- SpatialExperiment::addImg(spe,
+                                           sample_id = "sample01", #TODO? different samples get appended to cell_ids
+                                           image_id = img@name,
+                                           imageSource = img@file_path,
+                                           scaleFactor = NA_real_,
+                                           load = TRUE)
+        }
+        else{
+          message("\t - Skipping image with NULL file path")
+        }
+        S4Vectors::metadata(spe)[[img@name]] <- img
+      }
+    } else{
+      message("No spatial images found in the input Giotto object")
+    }
+
+    message("")
     # Add spe for current spatial unit to speList
     speList[[su]] <- spe
   }
 
- #  # Cell Metadata to ColData
- #  if(nrow(pDataDT(giottoObj)) > 0){
- #    message("Copying phenotype data")
- #    pData <- pDataDT(giottoObj)
- #    SummarizedExperiment::colData(spe) <- S4Vectors::DataFrame(pData, row.names = pData$cell_ID)
- #  } else{
- #    message("No phenotype data found in input Giotto object")
- #  }
- #
- #  # Feature Metadata to RowData
- #  if(nrow(fDataDT(giottoObj)) > 0){
- #    message("Copying feature metadata")
- #    SummarizedExperiment::rowData(spe) <- fDataDT(giottoObj)
- #  } else{
- #    message("No feature metadata found in input Giotto object")
- #  }
- #
- #  # Spatial Locations to Spatial Coordinates
- #  if(!is.null(get_spatial_locations(giottoObj))){
- #    message("Copying spatial locations")
- #    SpatialExperiment::spatialCoords(spe) <- data.matrix(get_spatial_locations(giottoObj)[, 1:2])
- #  } else{
- #    message("No spatial locations found in the input Giotto object")
- #  }
- #
- #  # DimReductions
- #  giottoReductions <- list_dim_reductions(giottoObj)
- #  if(!is.null(giottoReductions)){
- #    message("Copying reduced dimensions")
- #    for(i in seq(nrow(giottoReductions))){
- #      SingleCellExperiment::reducedDim(spe, giottoReductions[i]$name) <- get_dimReduction(
- #        gobject = giottoObj,
- #        reduction = "cells",
- #        spat_unit = giottoReductions[i]$spat_unit,
- #        feat_type = giottoReductions[i]$feat_type,
- #        reduction_method = giottoReductions[i]$dim_type,
- #        name = giottoReductions[i]$name)
- #    }
- #  } else{
- #    message("No reduced dimensions found in the input Giotto object")
- #  }
- #
- # # SpatialGrid
- #
- #  # NN Graph
- #  giottoNearestNetworks <- list_nearest_networks(giottoObj)
- #  if(!is.null(giottoNearestNetworks)){
- #    message("Copying nearest networks")
- #    for(i in seq(nrow(giottoNearestNetworks))){
- #      nn_network <- get_NearestNetwork(
- #        gobject = giottoObj,
- #        spat_unit = giottoNearestNetworks[i]$spat_unit,
- #        output = "data.table",
- #        nn_network_to_use = giottoNearestNetworks[i]$type,
- #        network_name = giottoNearestNetworks[i]$name)
- #
- #      # SPE stores in colpairs, with col indices instead of colnames
- #      cell1 <- match(nn_network$from, pDataDT(giottoObj)$cell_ID)
- #      cell2 <- match(nn_network$to, pDataDT(giottoObj)$cell_ID)
- #
- #      SingleCellExperiment::colPair(spe, giottoNearestNetworks[i]$name) <- S4Vectors::SelfHits(
- #        cell1, cell2, nnode=ncol(spe), nn_network[, -1:-2]) #removing from and to
- #    }
- #  } else{
- #    message("No nearest networks found in the input Giotto object")
- #  }
- #
- #  # Spatial Networks
- #  giottoSpatialNetworks <- list_spatial_networks(giottoObj)
- #  if(!is.null(giottoSpatialNetworks)){
- #    message("Copying spatial networks")
- #    for(i in seq(nrow(giottoSpatialNetworks))){
- #      sp_network <- get_spatialNetwork(gobject = giottoObj, spat_unit = giottoSpatialNetworks[i]$spat_unit, name = giottoSpatialNetworks[i]$name)
- #
- #      # spe stores in colpairs, with col indices instead of colnames
- #      cell1 <- match(sp_network$from, pDataDT(giottoObj)$cell_ID)
- #      cell2 <- match(sp_network$to, pDataDT(giottoObj)$cell_ID)
- #
- #      SingleCellExperiment::colPair(spe, giottoSpatialNetworks[i]$name) <- S4Vectors::SelfHits(
- #        cell1, cell2, nnode=ncol(spe), sp_network[, -1:-2]) #removing from and to
- #    }
- #  } else{
- #    message("No spatial networks found in the input Giotto object")
- #  }
- #
- #  # SpatialImages
- #  giottoImages <- list_images(giottoObj)
- #  if(!is.null(giottoImages)){
- #    message("Copying spatial images")
- #    for(i in seq(nrow(giottoImages))){
- #      img <- get_giottoImage(
- #        gobject = giottoObj,
- #        image_type = giottoImages[i]$img_type,
- #        name = giottoImages[i]$name)
- #
- #      if(!is.null(img@file_path)){
- #        spe <- SpatialExperiment::addImg(spe,
- #                                         sample_id = "sample01", #TODO? different samples get appended to cell_ids
- #                                         image_id = img@name,
- #                                         imageSource = img@file_path,
- #                                         scaleFactor = NA_real_,
- #                                         load = TRUE)
- #      }
- #      else{
- #        message("\t - Skipping image with NULL file path")
- #      }
- #      S4Vectors::metadata(spe)[[img@name]] <- img
- #    }
- #  } else{
- #    message("No spatial images found in the input Giotto object")
- #  }
-
-  # return SPE
+  # return list of spe objects
   return(speList)
 }
 
