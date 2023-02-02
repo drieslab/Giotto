e92536a870cc2f20d495c2e9731cd0f021caa14c
jiajic <72078254+jiajic@users.noreply.github.com>
2023-02-02 16:56:24 -0500

    create documentation script
    Add rd_t.R which holds commonly used param documentation to be inherited from

---
+++ R/rd_t.R
@@ -0,0 +1,40 @@
+
+
+# ------------------------------------------------------------------------- #
+# This script contains reusable dummy documentation for commonly used params
+# Use @inheritParams data_access when documenting these parameters
+# ------------------------------------------------------------------------- #
+
+
+#' @name data_access
+#' @param gobject giotto object
+#' @param spat_unit spatial unit (e.g. "cell")
+#' @param feat_type feature type (e.g. "rna", "dna", "protein")
+#' @param return_uniques return unique nesting names (ignores if final object exists/is correct class)
+#' @param output what format in which to get information (e.g. "data.table")
+#' @param set_defaults set default spat_unit and feat_type. Change to FALSE only when
+#' expression and spat_info are not expected to exist.
+#' @param copy_obj whether to deep copy/duplicate when getting the object (default = TRUE)
+#' @keywords internal
+NULL
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
