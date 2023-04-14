3189518c017fd4d0b2ecde0ef90baa7adc90d4ad
PratishthaGuckhool <58049857+PratishthaGuckhool@users.noreply.github.com>
2023-04-14 16:18:12 -0400

    Update cross_section.R

--- R/cross_section.R
+++ R/cross_section.R
@@ -112,7 +112,8 @@ estimateCellCellDistance <- function(gobject,
                                      method=c("mean","median")
                                      ){
 
-  delaunay_network_DT = gobject@spatial_network[[spatial_network_name]]$networkDT
+  delaunay_network_DT = gobject@spatial_network$cell$Delaunay_network
+  delaunay_network_DT = delaunay_network_DT@networkDT
 
   CellCellDistance = get_distance(networkDT= delaunay_network_DT,
                                               method=method)
