46914b52504ee77d04834c990c561380e1d94721
mattobny <mattobny@umich.edu>
2023-06-16 14:28:51 -0400

    Change h5 file handling

--- R/giotto.R
+++ R/giotto.R
@@ -1300,7 +1300,7 @@ createGiottoObject = function(expression,
                               cores = determine_cores(),
                               raw_exprs = NULL,
                               expression_matrix_class = c('dgCMatrix', 'HDF5Matrix','rhdf5'),
-                              h5_file = 'my_giotto_object.h5',
+                              h5_file = NULL,
                               verbose = FALSE) {
   
   debug_msg = FALSE # for reading debug help
@@ -1394,8 +1394,8 @@ createGiottoObject = function(expression,
                                    cores = cores,
                                    default_feat_type = expression_feat,
                                    verbose = debug_msg,
-                                   expression_matrix_class = expression_matrix_class,
-                                   h5_file = h5_file)
+                                   expression_matrix_class = expression_matrix_class)#,
+                                   #h5_file = h5_file)
     ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
     gobject = setExpression(gobject = gobject,
                             x = expression_data,
